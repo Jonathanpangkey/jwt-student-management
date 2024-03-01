6eb20d5b5d7daa536879d81d840461308a3a5416
Jonathan Pangkey <jonathanpangkey912@gmail.com>
2024-03-01 14:43:37 +0800

    initial commit

---
+++ src/main/java/com/example/springjwtsecurity/user/User.java
@@ -0,0 +1,26 @@
+package com.example.springjwtsecurity.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
